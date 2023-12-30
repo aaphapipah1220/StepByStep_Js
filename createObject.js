317a44d13e0b2611a21f6046fb65eaa734f771eb
aaphapipah1220 <aaphapipah@outlook.com>
2023-12-30 17:27:42 +0700

    add new materi object

--- createObject.js
+++ createObject.js
@@ -7,6 +7,25 @@ function halo() {
 }
 halo();
 
+// function Mahasiswa(nama, energi) {
+//   let mahasiswa = {};
+//   mahasiswa.nama = nama;
+//   mahasiswa.energi = energi;
+
+//   mahasiswa.makan = function (porsi) {
+//     this.energi += porsi;
+//     console.log(`met makan ${nama}`);
+//   };
+
+//   mahasiswa.main = function (jam) {
+//     this.energi -= jam;
+//     console.log(`yah main mulu si lu ${nama}`);
+//   };
+
+//   return mahasiswa;
+// }
+// let aap = Mahasiswa("Aap", 10);
+
 // cara 2 - object literal
 // this dalam literal mengembalikan nilai yang bersangkutan
 let obj = {};
@@ -15,9 +34,34 @@ obj.say = function () {
 };
 obj.say();
 
+// let human = {
+//   nama: "kucing",
+//   nyawa: 10,
+//   makan: function (porsi) {
+//     this.nyawa += porsi;
+//     console.log(`hi bro ${this.nama} selamat makan yup!`);
+//   },
+// };
+
 // cara 3 - constructor
 // this dalam constructor mengembalikan nilai yang baru
 function said() {
   console.log("this one example from constructor");
 }
 new said();
+
+function Mahasiswa(nama, energi) {
+  this.nama = nama;
+  this.energi = energi;
+
+  this.makan = function (porsi) {
+    this.energi += porsi;
+    console.log(`met makan ${nama}`);
+  };
+
+  this.main = function (jam) {
+    this.energi -= jam;
+    console.log(`yah main mulu si lu ${nama}`);
+  };
+}
+let aap = new Mahasiswa("Aap", 10);
